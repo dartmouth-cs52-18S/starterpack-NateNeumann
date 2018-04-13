a47fdaca2c89b9cf1466a263acaa834f52b6095e
NateNeumann <nathan.j.neumann.20@dartmouth.edu>
2018-04-12 20:37:16 -0400

    Release V1 commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 1;
+setInterval(() => {
+  $('#main').html(`You have been on this webpage for ${num} seconds.`);
+  num += 1;
+}, 1000);
